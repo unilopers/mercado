23a7dc2fe3a0287a0995282046e793e5fcc4d295
JulioAlmeron <163451291+JulioAlmeron@users.noreply.github.com>
2025-11-25 20:33:36 -0300

    ✨ feat: Implementação da entidade Categoria.java

--- src/main/java/com/unilopers/mercado/model/Categoria.java
+++ src/main/java/com/unilopers/mercado/model/Categoria.java
@@ -1,5 +1,33 @@
 package com.unilopers.mercado.model;
-
+import jakarta.persistence.*;
+import lombok.Data;
+@Entity
+@Table(name = "categoria")
 public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome_categoria", nullable = false)
+    private String categoria;
 
+    public  Categoria() {
+    }
+    public Categoria(String categoria) {
+        this.categoria = categoria;
+    }
+    public Long getId() {
+        return id;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 }
