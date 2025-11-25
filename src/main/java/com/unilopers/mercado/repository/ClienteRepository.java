2efa24da9069ee4d60ad3b8ce20b64d190b8b897
kkawan <159156218+kkawan@users.noreply.github.com>
2025-11-25 09:40:14 -0300

    ✨ feat: implementação do ProdutoRepository.java

--- src/main/java/com/unilopers/mercado/repository/ClienteRepository.java
+++ src/main/java/com/unilopers/mercado/repository/ClienteRepository.java
@@ -1,4 +1,9 @@
 package com.unilopers.mercado.repository;
 
-public class ClienteRepository {
+import com.unilopers.mercado.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
