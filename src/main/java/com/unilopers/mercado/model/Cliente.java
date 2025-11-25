ac4fbb3a8d8ffe16431987793893d81fb94b1453
kkawan <159156218+kkawan@users.noreply.github.com>
2025-11-25 09:20:08 -0300

    ✨ feat: implementação da entidade Cliente

--- src/main/java/com/unilopers/mercado/model/Cliente.java
+++ src/main/java/com/unilopers/mercado/model/Cliente.java
@@ -1,4 +1,17 @@
 package com.unilopers.mercado.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "clientes")
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
 }
