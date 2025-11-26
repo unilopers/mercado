fce25a9fe9db7ddfc4455303bab2b896e1c46f09
JulioAlmeron <163451291+JulioAlmeron@users.noreply.github.com>
2025-11-25 21:22:39 -0300

    Revert "✨ feat: Implementação do CategoriaController.java"
    This reverts commit 9d87bbad57ed929ce1083afa4a50c787ebf20785.

--- src/main/java/com/unilopers/mercado/repository/CategoriaRepository.java
+++ src/main/java/com/unilopers/mercado/repository/CategoriaRepository.java
@@ -1,10 +1,5 @@
 package com.unilopers.mercado.repository;
 
-import com.unilopers.mercado.model.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
+public class CategoriaRepository {
 
-import java.util.Optional;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-    Optional<Categoria> findByCategoria(String categoria);
 }
