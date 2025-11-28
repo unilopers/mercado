09a4ea20e1e9b72db0f67e8097d8cd71dd0883b3
kkawan <159156218+kkawan@users.noreply.github.com>
2025-11-28 17:32:26 -0300

    🔒️ Adiciona validação ao modelo Cliente
    Adiciona validação de nome e email para os campos do cliente. Deve ser feito um exception futuramente para mostrar as mensagens

--- src/main/java/com/unilopers/mercado/model/Cliente.java
+++ src/main/java/com/unilopers/mercado/model/Cliente.java
@@ -2,6 +2,7 @@ package com.unilopers.mercado.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import jakarta.validation.constraints.*;
 
 @Data
 @Entity
@@ -12,7 +13,13 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(min = 3, max = 100, message = "Nome deve ter entre 3 e 100 caracteres")
     private String nome;
+
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email deve ser válido")
+    @Column(unique = true)
     private String email;
 
 }
