8cb1f9e198b956a643056239fd27c3679be213cb
Mariana Yumi Morishita Yokomizo <mariyumiyokomizo@gmail.com>
2025-12-01 16:35:56 -0300

    ♻️refactor: refatoração do ResumoPedidoController.java

--- src/main/java/com/unilopers/mercado/controller/ResumoPedidoController.java
+++ src/main/java/com/unilopers/mercado/controller/ResumoPedidoController.java
@@ -1,56 +1,21 @@
 package com.unilopers.mercado.controller;
 
-import java.math.BigDecimal;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import com.unilopers.mercado.model.Pedido;
 import com.unilopers.mercado.model.ResumoPedido;
-import com.unilopers.mercado.model.Item_pedido;
-import com.unilopers.mercado.repository.Item_pedidoRepository;
-import com.unilopers.mercado.repository.PedidoRepository;
-import com.unilopers.mercado.repository.ResumoPedidoRepository;
+import com.unilopers.mercado.service.ResumoPedidoService;
 
 @RestController
 @RequestMapping("/resumo")
 public class ResumoPedidoController {
 
     @Autowired
-    private PedidoRepository pedidoRepository;
-
-    @Autowired
-    private ResumoPedidoRepository resumoRepository;
-
-    @Autowired
-    private Item_pedidoRepository itemPedidoRepository;
+    private ResumoPedidoService resumoService;
 
     @PostMapping("/gerar/{idPedido}")
     public ResumoPedido gerarResumo(@PathVariable Long idPedido) {
-
-        Pedido pedido = pedidoRepository.findById(idPedido)
-                .orElseThrow(() -> new RuntimeException("Pedido não encontrado"));
-
-        // Busca os itens do pedido diretamente da tabela item_pedido
-        List<Item_pedido> itens = itemPedidoRepository.findByPedidoIdPedido(idPedido);
-
-        ResumoPedido resumo = new ResumoPedido();
-        resumo.setPedido(pedido);
-
-        int totalItens = itens.stream()
-                .mapToInt(Item_pedido::getQuantidade)
-                .sum();
-        resumo.setQuantidadeItens(totalItens);
-
-        BigDecimal totalBruto = BigDecimal.valueOf(pedido.getPrecoTotal());
-        resumo.setValorTotalBruto(totalBruto);
-
-        BigDecimal impostos = totalBruto.multiply(BigDecimal.valueOf(0.10));
-        resumo.setValorImpostos(impostos);
-
-        resumo.setValorTotalFinal(totalBruto.add(impostos));
-
-        return resumoRepository.save(resumo);
+        return resumoService.gerarResumo(idPedido);
     }
 }
+
